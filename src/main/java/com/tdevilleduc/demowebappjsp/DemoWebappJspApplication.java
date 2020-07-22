79ed4d1c6b893850aa78781acc609a956a57b7db
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-07-22 20:02:34 +0200

    init project

---
+++ src/main/java/com/tdevilleduc/demowebappjsp/DemoWebappJspApplication.java
@@ -0,0 +1,13 @@
+package com.tdevilleduc.demowebappjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebappJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebappJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdevilleduc/demowebappjsp/DemoWebappJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdevilleduc.demowebappjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebappJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
